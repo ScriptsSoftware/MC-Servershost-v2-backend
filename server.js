d5eb0d7ba25c51e523bc0f2f11a1207acb768210
ScriptsSoftware <dereckr970@gmail.com>
2026-06-22 16:20:11 -0600

    Add API routes

--- server.js
+++ server.js
@@ -1,134 +1,42 @@
 const express = require("express");
 const cors = require("cors");
-const fs = require("fs");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+let servidores = require("./servidores.json");
 
-let servers = [];
+app.get("/", (req, res) => {
+    res.send("⛏️ MC-Servershost V2 API activa");
+});
 
-if (fs.existsSync("servers.json")) {
-    servers = JSON.parse(
-        fs.readFileSync("servers.json")
-    );
-}
+app.get("/servidores", (req, res) => {
+    res.json(servidores);
+});
 
+app.post("/crear", (req, res) => {
 
-function save(){
-    fs.writeFileSync(
-        "servers.json",
-        JSON.stringify(servers, null, 2)
-    );
-}
-
-
-// Crear servidor
-app.post("/crear",(req,res)=>{
-
-    let servidor = {
-
+    const nuevoServidor = {
         id: Date.now(),
-
         nombre: req.body.nombre,
-
-        edition: req.body.edition,
-
+        tipo: req.body.tipo,
         version: req.body.version,
-
-        estado:"apagado"
-
+        estado: "apagado"
     };
 
-
-    servers.push(servidor);
-
-    save();
-
-
-    res.json(servidor);
-
-});
-
-
-// Ver servidores
-app.get("/servers",(req,res)=>{
-
-    res.json(servers);
-
-});
-
-
-// Encender
-app.post("/start/:id",(req,res)=>{
-
-    let s = servers.find(
-        x => x.id == req.params.id
-    );
-
-
-    if(s){
-
-        s.estado="encendido";
-
-        save();
-
-    }
-
-
-    res.json(s);
-
-});
-
-
-// Apagar
-app.post("/stop/:id",(req,res)=>{
-
-    let s = servers.find(
-        x => x.id == req.params.id
-    );
-
-
-    if(s){
-
-        s.estado="apagado";
-
-        save();
-
-    }
-
-
-    res.json(s);
-
-});
-
-
-// Eliminar
-app.delete("/delete/:id",(req,res)=>{
-
-
-    servers = servers.filter(
-        x => x.id != req.params.id
-    );
-
-
-    save();
-
+    servidores.push(nuevoServidor);
 
     res.json({
-        mensaje:"Servidor eliminado"
+        mensaje: "Servidor creado",
+        servidor: nuevoServidor
     });
-
 });
 
 
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000,()=>{
-
-console.log(
-"MC-Servershost V2 activo :3000"
-);
-
+app.listen(PORT, () => {
+    console.log("MC-Servershost V2 activo :" + PORT);
 });
